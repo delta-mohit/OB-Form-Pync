a25264c407b04eac9651699add002f79e7a35a6f
Mohit Saini <msmohit1612@gmail.com>
2024-12-15 12:24:47 +0530

    Remove brand and model dropdown

--- components/CarDetailsForm.jsx
+++ components/CarDetailsForm.jsx
@@ -39,7 +39,7 @@ const dataD = {
 const CarDetailsForm = ({ updateFields, formData }) => {
   return (
     <form className="w-full sm:w-[60%] mx-auto mt-2 sm:mt-10 font-montserrat">
-      <div className="grid md:grid-cols-2 md:gap-6">
+      {/* <div className="grid md:grid-cols-2 md:gap-6">
         <div className="w-full mx-auto mb-1 sm:mb-5">
           <DropdownMenu
             data={dataA}
@@ -55,6 +55,47 @@ const CarDetailsForm = ({ updateFields, formData }) => {
             updateFields={updateFields}
             fieldToUpdate={"model"}
           />
+        </div>
+      </div> */}
+
+      <div className="grid md:grid-cols-2 md:gap-6">
+        <div className="relative z-0 w-full mb-1 sm:mb-5 group">
+          <input
+            type="text"
+            name="floating_phone"
+            id="floating_phone"
+            className="block py-2.5 px-0 w-full text-[16px] text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            autoComplete="off"
+            value={formData.brand}
+            onChange={(e) => updateFields("brand", e.target.value)}
+            required
+          />
+          <label
+            htmlFor="floating_phone"
+            className="peer-focus:font-medium absolute text-[16px] text-gray-500 duration-300 transform -translate-y-5 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5"
+          >
+            Brand
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-1 sm:mb-5 group">
+          <input
+            type="text"
+            name="floating_company"
+            id="floating_company"
+            className="block py-2.5 px-0 w-full text-[16px] text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            autoComplete="off"
+            value={formData.model}
+            onChange={(e) => updateFields("model", e.target.value)}
+            required
+          />
+          <label
+            htmlFor="floating_company"
+            className="peer-focus:font-medium absolute text-[16px] text-gray-500 duration-300 transform -translate-y-5 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5"
+          >
+            Model
+          </label>
         </div>
       </div>
 
